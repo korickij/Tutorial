760e5a61f7eac24d7129b199e4b53c35b6b4e3fa
korickij <76440159+korickij@users.noreply.github.com>
2023-05-08 16:08:37 +0300

    clean-folder

---
+++ clean-folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean-folder',
+    version='1',
+    description='clean folder',
+    url='https://github.com/korickij/Tutorial/blob/main/proga.py',
+    author='Ivan Korickij',
+    author_email='kitmobi@gmail.com',
+    license='GoIT',
+    packages=find_namespace_packages()
+)
